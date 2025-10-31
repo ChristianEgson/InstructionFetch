3cd7647d59b3804b6aaa804f4c884961bdcd5b08
ChristianEgson <christianegson@gmail.com>
2025-10-30 22:39:20 -0700

    Fix: Match updated version of source file. Add reset functionality to if_id module

--- if_id.v
+++ if_id.v
@@ -22,15 +22,21 @@
 
 module if_id(
     input wire        clk,
+    input wire        reset,
     input wire [31:0] instr,
     input wire [31:0] npc,
     output reg [31:0] instrout,
     output reg [31:0] npcout
 );
    
-   always @(posedge clk) begin 
-        instrout <= instr;
-        npcout <= npc;
+   always @(posedge clk or posedge reset) begin 
+        if (reset) begin
+            instrout <= 0;
+            npcout   <= 0;
+        end else begin
+            instrout <= instr;
+            npcout <= npc;
+        end
    end    
     
 endmodule
