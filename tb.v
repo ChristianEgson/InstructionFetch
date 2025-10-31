64973b66da3a8e1422592e6d80d7db185a319b9e
ChristianEgson <christianegson@gmail.com>
2025-10-30 23:44:05 -0700

    Refine comments in tb.v testbench
    Updated comments for clarity and corrected reset signal description.

--- tb.v
+++ tb.v
@@ -5,7 +5,7 @@ module tb_ifetch;
   reg [31:0] EX_MEM_NPC = 32'h0;
   wire [31:0] IF_ID_instr, IF_ID_npc;
 
-  always #5 clk = ~clk;
+  always #5 clk = ~clk; //100MHz
 
   I_FETCH dut (
     .clk(clk),
@@ -20,7 +20,8 @@ module tb_ifetch;
     $dumpfile("ifetch_tb.vcd");
     $dumpvars(0, tb_ifetch);
     
-    repeat(2) @(posedge clk); reset=0; //reset is low (active) for first 2 rising clock cycles
+    repeat(2) @(posedge clk); 
+    reset=0; //reset is high (active) for first 2 rising clock cycles, then made low
     repeat(4) @(posedge clk); //4 rising edge clk cycles of normal operation
     
     EX_MEM_NPC=32'h20; //branch target address set
