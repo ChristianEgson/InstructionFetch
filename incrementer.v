f52e3e2acfcffd03247a08d1ac81e6cfbdd12be3
ChristianEgson <christianegson@gmail.com>
2025-10-30 22:40:42 -0700

    Fix: Alter incrementer.v to match updated version of the source file. Refactor incrementer module to include clk and reset

--- incrementer.v
+++ incrementer.v
@@ -1,8 +1,15 @@
 module incrementer(
-    output wire [31:0] Next_PC,
+    input  wire        clk,
+    input  wire        reset, 
+    output reg [31:0] Next_PC,
     input  wire [31:0] PC 
 );
 
-    assign Next_PC = PC + 32'd4;
-    
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            Next_PC <= 0;
+        end else begin
+            Next_PC <= PC + 32'd4;
+        end
+    end 
 endmodule
